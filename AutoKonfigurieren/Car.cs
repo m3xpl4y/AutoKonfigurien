f7f1a2651019309008d3a103ddd39895220284a6
m3xpl4y <m3xpl4y@gmail.com>
2021-04-18 04:39:52 +0200

    first commit

---
+++ AutoKonfigurieren/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKonfigurieren
+{
+    class Car
+    {
+        private string Brand;
+        private int horesePower;
+        private int price;
+        private enum FUELTYPE { DIESEL, GAS}
+
+
+    }
+}
---
+++ AutoKonfigurieren/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+
+namespace AutoKonfigurieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Maximilian";
+            var filePath = @"C:\\Database\\maxfile.txt";
+            var txt = new StringBuilder();
+            var newLine = string.Format("{0}", name);
+            txt.AppendLine(newLine);
+            File.AppendAllText(filePath, txt.ToString());
+        }
+    }
+}
